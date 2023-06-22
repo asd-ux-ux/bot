f322630d1f8f54579aa6d7ff910e091ffc472b1f
asd-ux-ux <74491743+asd-ux-ux@users.noreply.github.com>
2023-06-22 21:33:03 +0500

    Update and rename asd.cjs to asd.mjs
    asd

--- asd.mjs
+++ asd.mjs
@@ -1,4 +1,4 @@
-const fetch = (...args) => import('node-fetch').then(({default: fetch}) => fetch(...args));
+import fetch from "node-fetch";
 
 let url = "https://api.telegram.org/bot5603085243:AAFAyrax39qiK_yQkG3_S0q2EagrcFeywYg/";
 let started = [];
